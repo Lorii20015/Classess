035c788545e0a1d4c5078ef9386306c2385c44b8
Laura Bilíkova <bilikovalaura@bilikova.onmicrosoft.com>
2021-02-06 18:42:05 +0100

    Found out how to make github work.

---
+++ Classes/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Ball
+    {
+        public int BallSize;
+        public int BallSpeed;
+        public string BallColor;
+        public string BallType;
+        public string BallFact;
+        public Ball(int BSize, int BSpeed, string BColor, string BType, string BFact)
+        {
+            BallSize = BSize;
+            BallSpeed = BSpeed;
+            BallColor = BColor;
+            BallType = BType;
+            BallFact = BFact;
+        }
+      /*public string GetSize()
+        {
+            if ()
+            {
+
+            }
+        } */
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball SoccerBall = new Ball(3, 2, "black 'n white colored ball", " ball that's used to play soccer", "I have nice symetry and a lot of pentagons!");
+            Ball FootballBall = new Ball(3, 2, "orange 'n white colored ball", " ball that's used to play football", "I have kinda funny shape!");
+            Ball PingpongBall = new Ball(1, 3, "usually white colored ball", " ball that's used for ping-pong", "I am super hard to catch!");
+            List<Ball> Balls = new List<Ball>();
+            Balls.Add(SoccerBall);
+            Balls.Add(FootballBall);
+            Balls.Add(PingpongBall);
+            for (int a = 0; a < Balls.Count; a++)
+            {
+                //Ball CurrentBall = Balls[a];
+                int SizeOfBall = Balls[a].BallSize;
+                int SpeedOfBall = Balls[a].BallSpeed;
+                string ColorOfBall = Balls[a].BallColor;
+                string FactOfBall = Balls[a].BallFact;
+                string bsize = "";
+                string bspeed = "";
+                if (SizeOfBall == 1)
+                {
+                    bsize = "not very";
+                }
+                if (SizeOfBall == 2)
+                {
+                    bsize = "kinda";
+                }
+                if (SizeOfBall == 3)
+                {
+                    bsize = "very";
+                }
+                if (SpeedOfBall == 1)
+                {
+                    bspeed = "not very";
+                }
+                if (SpeedOfBall == 2)
+                {
+                    bspeed = "kinda";
+                }
+                if (SpeedOfBall == 3)
+                {
+                    bspeed = "very";
+                }
+                Console.WriteLine("Hi, I'm a " + ColorOfBall + ". I'm " + bspeed + " fast, and " + bsize + " big! An interesting fact about me is that " + FactOfBall);
+            }
+        }
+    }       
+}
